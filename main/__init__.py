a739cf092416a038061e584dd8daaedba919709c
bzmind <77694696+bzmind@users.noreply.github.com>
2024-08-08 15:30:25 +0330

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -12,12 +12,12 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.WARNING)
 
 # variables
-API_ID = 3599592
-API_HASH = "80865dfca1e192f81931cbf61203cfe7"
-BOT_TOKEN = "5619111521:AAFrXFWYqnFT16cloXdga-x2YUn8-x8rUQI"
-SESSION = "BQBqOLtAFJkA12k6Qx5sWenbV2Q4cIF44310BV7Lqv0d6nKqG-CWuKK7"
-FORCESUB = "DroneBots"
-AUTH = 1058482162
+API_ID = 25115384
+API_HASH = "1a53b1195b049269f8ab21d65a5864b3"
+BOT_TOKEN = "7212205658:AAGW5fXOzIVWm5gyjBRNkmrQNgk5kwuQuvg"
+SESSION = "BAF_OvgAvL-7f-JWiQSh6tKWngpcTvWxiZ2mQ02YZgzL0soQ7kJyU7QprHn0CM9lQGHNOQYuHYIM_l5EdxlPhasdXB1LxaBMHwhHSR3vHklSYZQUGtpSMc0hY-_bIYXyYxz_xMLEVJM2pekxyDhTBNTXx_6jFOZef2ey4eRyVuoItZLOeEbJhAMaAu8vW_fI8-dW98H5Ee8sKoDLkmwCWw6Y7qRYWYCsrsFQ8jjispiTEHMd-8U9N4JLo2lqLtMW3lpFp1iDqI1eV3HwcfPc23lHAemzvZjuj-lpyiQXTamrCHBLgrz5YrHoyKT0BOb_UOLgtjm8EHhskyIBiwrLjcMq9PzgfQAAAAGt4YZaAQ"
+FORCESUB = "melodicmind"
+AUTH = 7369755354
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
 
